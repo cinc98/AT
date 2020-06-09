1d7b4e017cc7b1f5e94cdb9f4e93342bb212a6b9
cinc98 <ivan.cincurak98@gmail.com>
2020-06-09 16:07:47 +0200

    initial client project and added cors filter

---
+++ at-client/src/components/HelloWorld.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <button @click="click">asd</button>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String
+  },
+  methods: {
+    click() {
+      axios
+        .post(`http://localhost:8080/ATProjectWAR/rest/chat/post/dadas`)
+        .then(response => {
+          alert(response.data);
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    }
+  },
+  created() {
+    this.socket = new WebSocket(`ws://localhost:8080/ATProjectWAR/ws`);
+    this.socket.onopen = function(event) {
+      console.log(event);
+    };
+    this.socket.onmessage = function(event) {
+      console.log(event);
+    };
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
